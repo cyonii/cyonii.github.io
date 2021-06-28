c50d17eaa90a38fb565ea0dd7db3b7ff9e68d2f4
CY Kalu <cy23@tutanota.com>
2021-06-28 11:58:41 +0000

    Drop node

--- src/index.js
+++ src/index.js
@@ -1,10 +1,54 @@
-import './scss/master.scss';
-const stack = require('./js/stack.json');
+const stack = [
+  'Fullstack',
+  'Freelance',
+  'Python / Django',
+  'Ruby / Rails',
+  'JS / React',
+  'Bootstrap',
+  'HTML',
+  'CSS(SASS)',
+];
+const body = document.body;
+const navToggler = document.getElementById('navbar-menu-toggler');
+const navbarCollapse = document.querySelector('.navbar-collapse');
 
-const landingPage = document.getElementById('landing');
-const experiment = document.getElementById('experiment');
+// Toggle Navigation Menu - Mobile
+navToggler.addEventListener('click', (event) => {
+  event.stopPropagation();
+  menuIsOpen() ? closeMenu(event) : showMenu(event);
+});
+
+function showMenu(event) {
+  navbarCollapse.classList.add('show');
+  navToggler.classList.add('close');
+  body.classList.add('menu-open');
+  window.addEventListener('click', closeMenu);
+}
+
+function closeMenu(event) {
+  if (event.target !== navbarCollapse) {
+    navbarCollapse.classList.remove('show');
+    navToggler.classList.remove('close');
+    body.classList.remove('menu-open');
+    window.removeEventListener('click', closeMenu);
+  }
+}
+
+function menuIsOpen() {
+  return navbarCollapse.classList.contains('show');
+}
+
+// Hide call to scroll on scrool
+window.addEventListener('scroll', () => {
+  let scrollCall = document.getElementById('call-to-scroll');
+
+  window.scrollY > 100
+    ? (scrollCall.style.visibility = 'hidden')
+    : (scrollCall.style.visibility = 'visible');
+});
 
 // Add background objects
+const landingPage = document.getElementById('landing');
 for (let i = 1; i < 20; i++) {
   const object = document.createElement('div');
 
@@ -13,6 +57,7 @@ for (let i = 1; i < 20; i++) {
 }
 
 // Engrave stack
+const experiment = document.getElementById('experiment');
 let stackIndex = 0;
 let textIndex = 0;
 let featuredStack = '';
@@ -23,6 +68,8 @@ function engraveStack() {
   featuredStack = stack[stackIndex];
   span.style.color = '#fff';
   span.append(featuredStack[textIndex]);
+  span.onmouseenter = (event) => console.log(event);
+
   experiment.appendChild(span);
   textIndex++;
 
@@ -74,4 +121,40 @@ stackTogglers.forEach((toggler) => {
       }
     }
   });
+});
+
+// Carousel ride
+function getCarouselItems(el) {
+  const items = Array.from(el.querySelectorAll('.carousel-inner .carousel-item'));
+  const activeItem = items.find((i) => i.classList.contains('active'));
+  const nextItem = activeItem.nextElementSibling;
+  const prevItem = activeItem.previousElementSibling;
+
+  return { items, activeItem, nextItem, prevItem };
+}
+
+const carouselControls = document.querySelectorAll('[data-slide-target]');
+carouselControls.forEach((control) => {
+  control.onclick = (e) => {
+    e.preventDefault();
+
+    const carouselID = e.currentTarget.getAttribute('data-slide-target');
+    const carousel = document.querySelector(carouselID);
+    const { items, activeItem, nextItem, prevItem } = getCarouselItems(carousel);
+    const slideControl = e.currentTarget.getAttribute('data-slide-ctrl');
+
+    if (items.length < 2) return; // return if only 1 .carosuel-item exists
+
+    activeItem.classList.remove('active');
+    switch (slideControl) {
+      case 'next':
+        if (nextItem) nextItem.classList.add('active');
+        else items[0].classList.add('active');
+        break;
+      case 'prev':
+        if (prevItem) prevItem.classList.add('active');
+        else items[items.length - 1].classList.add('active');
+        break;
+    }
+  };
 });
