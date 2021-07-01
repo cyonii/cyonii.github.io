eb69131f362cc9c34899a3364ab0d44472d71593
CY Kalu <cy23@tutanota.com>
2021-07-01 19:10:47 +0000

    Add py script to generate template

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
+from pathlib import Path
+from shutil import rmtree, copytree
 import jinja2
-import os
 
-loader = jinja2.FileSystemLoader('templates')
-environment = jinja2.Environment(loader=loader)
-sample = environment.get_template('index.html')
 
-print(sample.render())
+BASE_DIR = Path.cwd()
+OUTPUT_PATH = BASE_DIR / 'dist'
+FILE_PATH = OUTPUT_PATH / 'index.html'
+STATIC_DIR = BASE_DIR / 'assets' / 'static'
+STATIC_OUTPUT = BASE_DIR / 'assets' / 'static'
+
+
+def compile():
+    loader = jinja2.FileSystemLoader('templates')
+    environment = jinja2.Environment(loader=loader)
+    sample = environment.get_template('index.html')
+
+    try:
+        rmtree(OUTPUT_PATH)
+    except FileNotFoundError:
+        pass
+    Path.mkdir(OUTPUT_PATH)
+    Path.touch(FILE_PATH)
+
+    with open(FILE_PATH, 'w') as file:
+        file.write(sample.render())
+
+    copytree(STATIC_DIR, OUTPUT_PATH / 'static')
+
+
+compile()
